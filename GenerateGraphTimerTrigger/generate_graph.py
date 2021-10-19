b8f0c6a92dd254c58877908bd032047f0a9fddb6
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2021-10-19 19:21:45 +0530

    Master (#3)
    * fixed import error
    
    * added pandas to requirements.txt
    
    * bug fix in prepare_df
    
    * swapping fig names
    
    * use png for image format while storing it in blob
    
    * fixing error for image overlap
    
    * added buffer flush and close
    
    * seperate io r/w for image generation

--- GenerateGraphTimerTrigger/generate_graph.py
+++ GenerateGraphTimerTrigger/generate_graph.py
@@ -10,9 +10,9 @@ class GenerateGraph:
         self.cosmos_db_client_obj = cosmos_db_client_obj
 
         self.blob_client_obj_vaccine_status = BlobStorageClient(
-            "covid-19-trend-graph", "vaccine_status_graph")
+            "covid-19-trend-graph", "vaccine_status_graph.png")
         self.blob_client_obj_vaccine_name = BlobStorageClient(
-            "covid-19-trend-graph", "vaccine_name_graph")
+            "covid-19-trend-graph", "vaccine_name_graph.png")
         
         self.vaccine_status_df, self.vaccine_name_df = None, None
         
@@ -40,46 +40,67 @@ class GenerateGraph:
         
         logging.info("Gathered the data and created the dataframe")
 
-    def generate_graphs(self):
+    def generate_graph_vaccine_status(self):
         
-        vaccine_name_fig = self.vaccine_status_df.plot.pie(y="vaccine_name", figsize=(5, 5),
-                                          legend="vaccination_status", title="Vaccination Status",
+        vaccine_status_fig = self.vaccine_status_df.plot.pie(y="vaccination_status", figsize=(5, 5),
+                                          legend="vaccination_status", title="By Vaccination Status",
                                           autopct=lambda p: '{:.0f}'.format(
                                               (p/100)*self.vaccine_status_df.sum()),
                                           cmap="spring")
 
+        image_object = vaccine_status_fig.get_figure()
 
-        vaccine_status_fig = self.vaccine_name_df.plot.pie(y="vaccine_name", figsize=(5, 5),
+        buf = io.BytesIO()
+        image_object.savefig(buf, format='png')
+        byte_im = buf.getvalue()
+        self.blob_client_obj_vaccine_status.blob_client.upload_blob(
+            byte_im, overwrite=True, blob_type="BlockBlob")
+        buf.flush()
+        buf.close()
+
+
+    def generate_graph_vaccination_name(self):
+        vaccine_name_fig = self.vaccine_name_df.plot.pie(y="vaccine_name", figsize=(5, 5),
                                     legend="vaccine_name", title="By Vaccine Name",
                                     autopct=lambda p: '{:.0f}'.format(
                                         (p/100)*self.vaccine_name_df.sum()),
                                     cmap="winter")
 
-        self.vaccine_name_image_object = vaccine_name_fig.get_figure()
-        self.vaccine_status_image_object = vaccine_status_fig.get_figure()
+        image_object = vaccine_name_fig.get_figure()
+        
+        buf = io.BytesIO()
+        image_object.savefig(buf, format='png')
+        byte_im = buf.getvalue()
+        self.blob_client_obj_vaccine_name.blob_client.upload_blob(
+            byte_im, overwrite=True, blob_type="BlockBlob")
+        buf.flush()
+        buf.close()
 
 
     def save_image_to_blob_storage(self, blob_client, image_object):
         buf = io.BytesIO()
-        image_object.save(buf, format='png')
+        image_object.savefig(buf, format='png')
         byte_im = buf.getvalue()        
-        blob_client.upload_blob(byte_im, overwrite=True, blob_type="BlockBlob")    
+        blob_client.upload_blob(
+            byte_im, overwrite=True, blob_type="BlockBlob")
         buf.flush()
-
+        buf.close()
+        
     def start_process(self):
         logging.info("Starting generate graph process")
         
         self.query_all_item_from_cosmos_db()
         
         self.prepare_df()
-        self.generate_graphs()
+        self.generate_graph_vaccine_status()
+        self.generate_graph_vaccination_name()
         
         logging.info("Successfully generated image objects")
         
-        self.save_image_to_blob_storage(
-            self.blob_client_obj_vaccine_status.blob_client, self.vaccine_status_image_object)
-        self.save_image_to_blob_storage(
-            self.blob_client_obj_vaccine_name.blob_client, self.vaccine_name_image_object)
+        # self.save_image_to_blob_storage(
+        #     self.blob_client_obj_vaccine_status.blob_client, self.vaccine_status_image_object)
+        # self.save_image_to_blob_storage(
+        #     self.blob_client_obj_vaccine_name.blob_client, self.vaccine_name_image_object)
         
         logging.info("Successfully uploaded image to blob storage")
         
