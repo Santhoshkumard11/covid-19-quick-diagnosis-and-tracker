b9a971bb9edb7fc4659194264ef73061b6d07061
Santhoshkumard11 <santhoshkdhana@gmail.com>
2021-10-30 22:12:55 +0530

    updated readme.md and added image

--- SavePatientDetails/save_patient_data.py
+++ SavePatientDetails/save_patient_data.py
@@ -2,14 +2,16 @@ from cosmos_db_client import CosmosDBClient
 
 import logging
 
-def save_patient_details(patent_json_date:dict):
-    
+
+def save_patient_details(patent_json_date: dict):
+
     try:
 
-        cosmos_db_client_obj = CosmosDBClient("covid-19-quick-diagnosis", "PatientTable","pincode")
+        cosmos_db_client_obj = CosmosDBClient(
+            "covid-19-quick-diagnosis", "PatientTable", "pincode")
 
         cosmos_db_client_obj.connect()
-        
+
         new_item_dict = {
             "name": patent_json_date.get("name"),
             "age": patent_json_date.get("age"),
@@ -20,16 +22,16 @@ def save_patient_details(patent_json_date:dict):
             "district": patent_json_date.get("district"),
             "pincode": patent_json_date.get("pincode"),
             "test_result": patent_json_date.get("test_result"),
-            "is_vaccinated_status": patent_json_date.get("is_vaccinated_status"),
+            "is_vaccinated_status": patent_json_date.get(
+                "is_vaccinated_status"),
             "vaccine_name": patent_json_date.get("vaccine_name")}
-        
+
         cosmos_db_client_obj.add_item(new_item_dict)
-        
+
         return "success", "none"
-    
+
     except Exception as e:
-        
+
         logging.exception("An exception occurred while saving user data")
-        
+
         return "failed", str(e)
-    
